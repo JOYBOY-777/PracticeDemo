48f3a93439448d6df13352d2b13b9c43228753ec
JOYBOY <1781073841@qq.com>
2022-11-17 14:45:10 +0800

    注解以及全局主键生成策略

--- mybatis_plus_demo/src/main/java/com/jin/pojo/User.java
+++ mybatis_plus_demo/src/main/java/com/jin/pojo/User.java
@@ -1,5 +1,8 @@
 package com.jin.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +10,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//@TableName("t_user")
 public class User {
+    //TableId就是将对应的字段指定为主键
+    //里面的value属性用于指定主键的字段（如果实体类中的属性与数据库中不一致的情况下，那么就要要求value中的值一致）
+    //设置自动递增
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String name;
     private Integer age;
--- mybatis_plus_demo/src/test/java/com/jin/MybatisPlusServiceTest.java
+++ mybatis_plus_demo/src/test/java/com/jin/MybatisPlusServiceTest.java
@@ -1,11 +1,16 @@
 package com.jin;
 
+import com.jin.pojo.User;
 import com.jin.service.Impl.UserServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -22,6 +27,13 @@ public class MybatisPlusServiceTest {
     @Test
     //批量添加多条数据
     public void insertMore(){
-
+        List<User> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+            user.setAge(20+i);
+            user.setName("韩立"+i);
+            list.add(user);
+        }
+        System.out.println(userService.saveBatch(list));
     }
 }
